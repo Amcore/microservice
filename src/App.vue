6d480797e7f5cb9f9b20c2f84587ecb505fbe7d3
panmm <panmm@guahao.com>
2019-11-12 13:49:26 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,11 @@
 </template>
 
 <script>
-  export default {
-    mounted() {
-      console.log("adfadf")
-    }
+export default {
+  mounted() {
+    console.log("adfadf")
   }
+}
 </script>
 
 <style>
