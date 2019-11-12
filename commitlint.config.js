81760de49d7286314068a7b61c38a13b59397e83
panmm <panmm@guahao.com>
2019-11-12 14:21:22 +0800

    test: commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,21 +1,20 @@
 module.exports = {
   extends: [
-    "@commitlint/config-conventional"
+    '@commitlint/config-conventional'
   ],
   rules: {
     'type-enum': [
       2,
       'always',
       [
-        'upd',
-        'feat',
-        'fix',
-        'refactor',
-        'docs',
-        'chore',
-        'style',
-        'revert'
+        'upd', // 更新某功能（不是 feat, 不是 fix）
+        'feat', // 新功能（feature）
+        'fix', // 修补bug
+        'refactor', // 重构（即不是新增功能，也不是修改bug的代码变动）
+        'docs', // 文档（documentation）
+        'style', // 格式（不影响代码运行的变动）
+        'revert' // 还原
       ]
-     ]
+    ]
   }
-};
+}
